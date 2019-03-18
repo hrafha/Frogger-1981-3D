851e3fb9f3016fdc33b10b245af7864e99f6c501
Rafhael Ronny <rafhaelronnyst@gmail.com>
2019-03-18 13:16:43 -0300

    Implementando a classe Turtle

---
+++ Assets/Scripts/Level/Turtle.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Scripts.Level
+{
+    public class Turtle : MonoBehaviour
+    {
+        
+        [Header("Submerge Delays")]
+        [SerializeField] private float timeToUp;
+        [SerializeField] private float timeToDown;
+
+        private float timer;
+        private float defaultHeight;
+        private float submerseHeight;
+
+        private bool submerse;
+
+        [Space]
+        public float speed;
+
+        private void Start()
+        {
+            if (timeToUp <= 0)
+                timeToUp = 0.5f;
+            if (timeToDown <= 0)
+                timeToDown = 3f;
+            defaultHeight = transform.position.y;
+            submerseHeight = -0.5f;
+        }
+
+        private void Update()
+        {
+            SubmergeUpdate();
+        }
+
+        private void SubmergeUpdate()
+        {
+            timer += Time.deltaTime;
+            if (!submerse)
+            {
+                if (transform.position.y > submerseHeight)
+                    transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+
+                if (timer > timeToUp)
+                {
+                    timer = 0;
+                    submerse = true;
+                }
+            }
+            else
+            {
+                if (transform.position.y < defaultHeight)
+                    transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+
+                if (timer > timeToDown)
+                {
+                    timer = 0;
+                    submerse = false;
+                }
+            }
+        }
+
+    }
+}
